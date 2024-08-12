d537f37d829c4ce3a11a8581588478d8cfecdbdc
Sema Topcu <ssemaatopcu.@gmail.com>
2024-08-12 18:52:06 +0300

    Initial Commit

---
+++ Vitrin/VitrinApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitrinApp.swift
+//  Vitrin
+//
+//  Created by Sema Topcu on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct VitrinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
