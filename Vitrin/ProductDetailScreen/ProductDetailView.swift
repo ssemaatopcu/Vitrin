2edf2de1eca6b960002fc04e0f0286fc7386138b
Sema Topcu <ssemaatopcu.@gmail.com>
2024-08-13 13:10:38 +0300

    improve Produc DetailView design for better aesthetics

--- Vitrin/ProductDetailScreen/ProductDetailView.swift
+++ Vitrin/ProductDetailScreen/ProductDetailView.swift
@@ -13,20 +13,36 @@ struct ProductDetailView: View {
     
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading) {
-                URLImage(urlString: product.image)
-                    .frame(height: 300)
+            VStack(alignment: .center) {
                 
+                URLImage(urlString: product.image)
+                    .frame(height: 200)
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                   // .cornerRadius(40)
+                                    
                 Text(product.title)
                     .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center) // Center the title
                     .padding(.top)
+                    .padding(.horizontal)
+                
+                Divider()
                 
                 Text("Price: $\(product.price, specifier: "%.2f")")
                     .font(.title2)
+                    .foregroundColor(.orange)
                     .padding(.vertical)
+                
+                Divider()
                 
                 Text(product.description)
                     .font(.body)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(8)
+                    .padding(.horizontal)
                 
                 Spacer()
             }
